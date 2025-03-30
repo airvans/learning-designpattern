3e741d2d7383397b47a64ab9c9e5a797c2fa2051
airvans <evanarugu@gmail.com>
2025-03-30 22:53:06 +0200

    nice

--- Designpattern/Decorator/Program.cs
+++ Designpattern/Decorator/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Microsoft.VisualBasic;
+
+public interface Coffee{
+
+   double cost();
+   void description();
+
+}
+
+
+class Damn{
+
+    static void Main(string[] args){
+
+
+    }
+
+}
+
+
+
+class Starter : Coffee
+{
+    public double cost()
+    {
+        return 0;
+    }
+
+    public void description()
+    {
+        Console.WriteLine("plain coffee");
+    }
+}
+class Mocha : Coffee
+{
+    
+    Coffee coffee;
+
+    public Mocha(Coffee coffee)
+    {
+        this.coffee = coffee;
+    }
+
+    public double cost()
+    {
+        return coffee.cost() + 5; 
+    }
+
+    public void description()
+    {
+        Console.WriteLine("coffee made with mocha");
+    }
+}
+
+class Soy : Coffee
+{
+    Coffee coffee;
+
+    public Soy(Coffee coffee)
+    {
+        this.coffee = coffee;
+    }
+
+    public double cost()
+    {
+        return coffee.cost() + 11; 
+    }
+
+    public void description()
+    {
+        Console.WriteLine("coffee made with soy");
+    }
+}
